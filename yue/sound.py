98379edd366acdd25991cdbc54ab6a98aae30e43
Nick Setzer <nsetzer@ccis.neu.edu>
2016-01-19 14:43:59 -0500

    when available use vlc for playback

--- yue/sound.py
+++ yue/sound.py
@@ -39,6 +39,14 @@ from yue.library import Library
 from enum import Enum
 import random
 
+try:
+    import yue.vlc as vlc
+    vlc_support = True
+except:
+    vlc_support = False
+
+isPosix = False
+
 class MediaState(Enum):
     not_ready = 0
     play = 1
@@ -67,7 +75,10 @@ class SoundManager(EventDispatcher):
 
     @staticmethod
     def init():
-        SoundManager.__instance = KivySoundManager()
+        if vlc_support:
+            SoundManager.__instance = VlcSoundManager()
+        else:
+            SoundManager.__instance = KivySoundManager()
 
     @staticmethod
     def instance():
@@ -91,7 +102,13 @@ class SoundManager(EventDispatcher):
         raise NotImplementedError()
 
     def playpause(self):
-        raise NotImplementedError()
+        """ toggle state of audio
+        """
+        state = self.state()
+        if state == MediaState.play:
+            self.pause()
+        else: # if state == MediaState.pause:
+            self.play()
 
     #def stop(self):
     #    raise NotImplementedError()
@@ -272,15 +289,6 @@ class KivySoundManager(SoundManager):
                 self.sound.stop()
                 self.setClock(False)
 
-    def playpause(self):
-        """ toggle state of audio
-        """
-        if self.sound is not None:
-            if self.sound.state == 'play':
-                self.pause()
-            else:
-                self.play()
-
     #def stop(self):
     #    if self.sound is not None:
     #        self.sound.stop()
@@ -336,8 +344,140 @@ class KivySoundManager(SoundManager):
         else:
             self.dispatch('on_song_end')
 
+class VlcSoundManager(SoundManager):
+    """Playback implementation of SoundManager using Kivy"""
+    __instance = None
+    def __init__(self):
+        super(VlcSoundManager, self).__init__()
+        self.volume = 0.5
+        self.duration = 100 # updated on load
 
+        self.clock_scheduled = False
+        self.clock_interval = 0.5 # in seconds
 
+        self.invoke();
+
+    def invoke(self):
+        """
+            If the initalization fails
+            an attempt can be made to restart it from the command line
+            by typing:
+                exec MpGlobal.Player.mp.invoke()
+        """
+        try:
+            self.__instance__ = None;
+            if isPosix:
+                #'--plugin-path=/usr/lib/vlc'
+                self.__instance__ = vlc.Instance('--plugin-path=%s'%Settings.POSIX_VLC_MODULE_PATH)
+            else:
+                self.__instance__ = vlc.Instance()
+        except Exception as e:
+            print "VLC instance Error: %s"%(e.args)
+
+        if self.__instance__ == None:
+            print "VLC instance Error: No Instance was created"
+        else:
+            try:
+                self.__player__ = self.__instance__.media_player_new()
+            except Exception as e:
+                print "VLC Player Error: %s"%(e.args)
+
+            self.setVolume( self.volume )
+
+        self.__media__ = None
+
+    def unload(self):
+        if self.__media__ is not None:
+            self.__media__ = None
+            self.setClock(False)
+
+    def load(self, song):
+
+        self.unload()
+        # TODO: if path is unicode, some files will actually
+        # be loaded correctly, even if there are unicode characters
+        # in the path. decoding as utf-8 seems to always work correctly
+        # at least on windows. further research is required.
+        path = song['path']
+        self.duration = song.get('duration',100)
+        if self.__media__ is None:
+            self.__media__ = self.__instance__.media_new(path)
+            self.__player__.set_media(self.__media__)
+            print("got here",self.__player__,self.__media__)
+
+    def play(self):
+        if self.__media__ is not None:
+            self.__player__.play()
+            self.setClock(True)
+
+    def pause(self):
+        """
+        kivy audio does not enable pausing, instead the current position
+        is recorded when the audio is stopped.
+        """
+        if self.__media__ is not None:
+            self.__player__.pause()
+            self.setClock(False)
+
+    #def stop(self):
+    #    if self.sound is not None:
+    #        self.sound.stop()
+
+    def seek(self,seconds):
+        if self.__player__ is not None:
+            self.__player__.set_time(int(seconds*1000))
+
+    def position(self):
+        """ return current position in seconds (float)"""
+        if self.__media__ != None :
+            t = self.__player__.get_time();
+            return t/1000
+        else:
+            return 0
+
+    def duration(self):
+        return self.duration
+
+    def setVolume(self,volume):
+
+        self.__player__.audio_set_volume( max(0,min(100,int(100*volume))) )
+        self.volume = volume
+
+    def getVolume(self):
+        return self.volume
+
+    def state(self):
+        if self.__player__ is None:
+            return MediaState.not_ready;
+
+        s = self.__player__.get_state();
+        print(s)
+        if s == vlc.State.NothingSpecial:
+            return MediaState.not_ready
+        elif s == vlc.State.Opening:
+            return MediaState.not_ready
+        elif s == vlc.State.Buffering:
+            return MediaState.not_ready
+        elif s == vlc.State.Playing:
+            return MediaState.play
+        elif s == vlc.State.Paused:
+            return MediaState.pause
+        elif s == vlc.State.Stopped:
+            return MediaState.not_ready
+        elif s == vlc.State.Ended:
+            return MediaState.not_ready
+        elif s == vlc.State.Error:
+            return MediaState.not_ready
+        return MediaState.not_ready
+
+    def setClock(self,state):
+
+        if self.clock_scheduled == False and state == True:
+            self.clock_scheduled = True
+            Clock.schedule_interval( self.on_song_tick_callback, self.clock_interval )
+        elif self.clock_scheduled == True and state == False:
+            self.clock_scheduled = False
+            Clock.unschedule( self.on_song_tick_callback )
 
 
 
