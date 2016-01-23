fdc78e822521b142c84bd1297f74a274cae8c7de
Nick Setzer <nsetzer@ccis.neu.edu>
2016-01-22 19:06:53 -0500

    sql db wrapper

---
+++ test/test_sqlite3.py
@@ -0,0 +1,47 @@
+
+import os,sys
+dirpath = os.path.dirname(os.path.abspath(__file__))
+dirpath = os.path.dirname(dirpath)
+sys.path.insert(0,dirpath)
+
+from yue.sqlstore import SQLStore, SQLView
+
+library_columns = [
+    ('uid','integer PRIMARY KEY AUTOINCREMENT'),
+    ('path',"text"),
+    ('artist',"text"),
+    ('composer',"text DEFAULT ''"),
+    ('album','text'),
+    ('title','text'),
+    ('genre',"text DEFAULT ''"),
+    ('year','integer DEFAULT 0'),
+    ('country',"text DEFAULT ''"),
+    ('lang',"text DEFAULT ''"),
+    ('comment',"text DEFAULT ''"),
+    ('album_index','integer DEFAULT 0'),
+    ('length','integer DEFAULT 0'),
+    ('last_played','integer DEFAULT 0'),
+    ('playcount','integer DEFAULT 0'),
+    ('rating','integer DEFAULT 0'),
+]
+
+dbpath = "test.db"
+if os.path.exists(dbpath):
+    os.remove(dbpath)
+create = not os.path.exists(dbpath)
+
+store = SQLStore(dbpath)
+view = SQLView(store,"library", library_columns)
+
+# self.store.conn.execute("INSERT INTO library (artist, album, title, path) VALUES ('artist1','album1','title1','path1')")
+
+view.insert(artist="foo",album="bar",title="baz")
+
+res = view.get(1)
+print(res)
+
+res = view.update(1,composer="none")
+print(res)
+
+res = view.get(1)
+print(res)
