3e246728b95b66b0dc7847f2ca2a5959162ba61d
Nick Setzer <nsetzer@ccis.neu.edu>
2016-02-02 19:41:38 -0500

    add notes on foreground service notification

--- test/notification/main.py
+++ test/notification/main.py
@@ -16,9 +16,10 @@ from plyer import notification
 from plyer.utils import platform
 from plyer.compat import PY2
 
-import os,sys
+from jnius import autoclass, cast
+
+import sys
 from subprocess import Popen
-from threading import Thread
 
 serviceport = 15123 # Todo select these at run time
 activityport = 15124
@@ -92,6 +93,69 @@ class NotificationDemoApp(App):
         self.stop_service()
 
         Logger.critical('example: exit')
+
+def update_service():
+    """
+    http://cheparev.com/kivy-recipe-service-customization/
+
+    this post demonstrates how it could be possible to update the
+    default android foreground service notification.
+    """
+
+    Context = autoclass('android.content.Context')
+    Intent = autoclass('android.content.Intent')
+    PendingIntent = autoclass('android.app.PendingIntent')
+    AndroidString = autoclass('java.lang.String')
+    NotificationBuilder = autoclass('android.app.Notification$Builder')
+
+    PythonService = autoclass('org.renpy.android.PythonService')
+    service = PythonService.mService
+    #Drawable = autoclass("{}.R$drawable".format(service.getPackageName()))
+    text = AndroidString("Test Text".encode('utf-8'))
+    message = AndroidString("Test Msg".encode('utf-8'))
+
+    #small_icon = getattr(Drawable, 'tray_small')
+    #large_icon_bitmap = get_scaled_icon('icon_filename_without_extension')
+
+    contentIntent = PendingIntent.getActivity(service, 0, Intent(service, service.getClass()), 0)
+    notification_builder = NotificationBuilder(service)
+    notification_builder.setContentTitle(text)
+    notification_builder.setContentText(message)
+    #notification_builder.setSmallIcon(small_icon)
+    #notification_builder.setLargeIcon(large_icon_bitmap)
+    notification_builder.setContentIntent(contentIntent)
+
+    notification = notification_builder.getNotification()
+
+    notification_service = service.getSystemService(Context.NOTIFICATION_SERVICE)
+    notification_service.notify(1, notification)
+
+
+def get_scaled_icon(icon):
+    """
+    icon : name of icon file (png) without extension
+
+    this function assumes that a 'Drawable' was regiseted, (see original post)
+    it should be possible to create a bitmap dynamically, using a bitmap
+    factory.
+
+    Bitmap bm = BitmapFactory.decodeResource(getResources(), R.drawable.image);
+    """
+    Dimen = autoclass("android.R$dimen")
+    Bitmap = autoclass("android.graphics.Bitmap")
+    PythonService = autoclass('org.renpy.android.PythonService')
+    service = PythonService.mService
+    Drawable = autoclass("{}.R$drawable".format(service.getPackageName()))
+
+    scaled_icon = getattr(Drawable, icon)
+    scaled_icon = cast("android.graphics.drawable.BitmapDrawable",
+                       service.getResources().getDrawable(scaled_icon))
+    scaled_icon = scaled_icon.getBitmap()
+
+    res = service.getResources()
+    height = res.getDimension(Dimen.notification_large_icon_height)
+    width = res.getDimension(Dimen.notification_large_icon_width)
+    return Bitmap.createScaledBitmap(scaled_icon, width, height, False)
 
 
 if __name__ == '__main__':
