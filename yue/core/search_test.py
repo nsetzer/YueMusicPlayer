5db584deb765b43f66b62011793c6b0e14c6f69d
Nick Setzer <nsetzer@ccis.neu.edu>
2016-02-06 21:07:00 -0500

    generate search test cases at run time.

--- yue/core/search_test.py
+++ yue/core/search_test.py
@@ -1,7 +1,12 @@
-#! cd ../.. && python2.7 setup.py cover
-#! cd ../.. && python2.7 setup.py test --test=search
+#! cd ../.. && python2.7 setup.py test
+
+"""
+todo: none of these tests actually check that the correct answer is returned,
+only that the answers for applying rules, vs using sql match.
+"""
+
 import unittest
-import os,sys
+import os
 
 from yue.core.library import Library
 from yue.core.search import PartialStringSearchRule, \
@@ -22,88 +27,101 @@ from yue.core.sqlstore import SQLStore
 
 DB_PATH = "./unittest.db"
 
-class TestLibrarySearch(unittest.TestCase):
-    """Examples for using the cEBFS library
+sqlstore = None
+sqlview = None
+
+def setUpModule():
+
+    if os.path.exists(DB_PATH):
+        os.remove(DB_PATH)
+
+    global sqlstore
+    global sqlview
+
+    sqlstore = SQLStore( DB_PATH )
+    library = Library( sqlstore )
+
+    for i in range(20):
+        song = {"artist":"art%d"%i,
+                "album" :"alb%d"%i,
+                "title" :"ttl%d"%i,
+                "path"  :"/path/%d"%i,
+                "playcount":i,
+                "year":i%21+1990}
+        library.insert(**song)
+
+    sqlview = library.song_view
+
+def tearDownModule():
+
+    global sqlstore
+    global sqlview
+
+    sqlstore.close()
+
+class TestSearchMeta(type):
     """
+    Build a Search Test class.
+    search test follow a forumla, compare output from two different search
+    methods. This class builds a Test Class where each method in the class
+    is a test the same test, with different parameters
 
-    def __init__(self,*args,**kwargs):
-        super(TestLibrarySearch,self).__init__(*args,**kwargs)
+    """
+    def __new__(cls, name, bases, dict):
 
-    def setUp(self):
-        #if os.path.exists(DB_PATH):
-        #    os.remove(DB_PATH)
-        pass
+        def gen_compare_test(rule):
+            def test(self):
+                global sqlview
+                s1 = set( song['uid'] for song in naive_search( sqlview, rule ) )
+                s2 = set( song['uid'] for song in sql_search( sqlview, rule ) )
+                self.assertEqual(s1, s2)
+            return test
 
-    def tearDown(self):
-        #if os.path.exists(DB_PATH):
-        #    os.remove(DB_PATH)
-        pass
+        def gen_compare_rule_test(rule1,rule2):
+            def test(self):
+                global sqlview
+                s1 = set( song['uid'] for song in sql_search( sqlview, rule1 ) )
+                s2 = set( song['uid'] for song in sql_search( sqlview, rule2 ) )
+                self.assertEqual(s1, s2)
+            return test
 
-    def compare( self, library, rule, msg=""):
-        """ compare the two search methods, verify they return the same results"""
-
-        sqlview = library.song_view
-        s1 = set( song['uid'] for song in naive_search( sqlview, rule ) )
-        s2 = set( song['uid'] for song in sql_search( sqlview, rule ) )
-
-        self.assertEqual(s1,s2,msg)
-
-    def compare_rules( self, library, rule1, rule2, msg=""):
-        """ compare the two search methods, verify they return the same results"""
-
-        sqlview = library.song_view
-        s1 = set( song['uid'] for song in sql_search( sqlview, rule1 ) )
-        s2 = set( song['uid'] for song in sql_search( sqlview, rule2 ) )
-
-        self.assertEqual(s1,s2,msg)
-
-    def test_library_create(self):
-
-        # removing / creating db is slow
-        # therefore it is only done for this test
-
-        if os.path.exists(DB_PATH):
-            os.remove(DB_PATH)
-
-        sqlstore = SQLStore( DB_PATH )
-        library = Library( sqlstore )
-
-        for i in range(20):
-            song = {"artist":"art%d"%i,
-                    "album" :"alb%d"%i,
-                    "title" :"ttl%d"%i,
-                    "path"  :"/path/%d"%i,
-                    "playcount":i,
-                    "year":i%21+1990}
-            library.insert(**song)
-
-        self.compare( library, PartialStringSearchRule('artist','art1'), 'partial')
-
-        self.compare( library, InvertedPartialStringSearchRule('artist','art1'), 'partial-inv')
-
-        self.compare( library, ExactSearchRule('artist','art1'), 'eq-string')
-        self.compare( library, ExactSearchRule('playcount',2000), 'eq-numeric')
-
-        self.compare( library, InvertedExactSearchRule('artist','art1'), 'eq-string-inv')
-        self.compare( library, InvertedExactSearchRule('playcount',2000), 'eq-numeric-inv')
-
-        self.compare( library, LessThanSearchRule('playcount',2000), 'lt')
-        self.compare( library, LessThanEqualSearchRule('playcount',2000), 'le')
-        self.compare( library, GreaterThanSearchRule('playcount',2000), 'gt')
-        self.compare( library, GreaterThanEqualSearchRule('playcount',2000), 'ge')
-
-        rng1 = RangeSearchRule('playcount',1995,2005);
-        self.compare( library, rng1, 'range')
+        rng1 = RangeSearchRule('playcount',1995,2005)
         rng2 = NotRangeSearchRule('playcount',1995,2005)
-        self.compare( library, rng2, 'not-range')
 
         # show that two rules combined using 'and' produce the expected result
-
         gt1=GreaterThanEqualSearchRule('playcount',1995)
         lt1=LessThanEqualSearchRule('playcount',2005)
-        self.compare_rules(library, AndSearchRule([gt1,lt1]), rng1, "and")
 
         # show that two rules combined using 'or' produce the correct result
         lt2=LessThanSearchRule('playcount',1995)
         gt2=GreaterThanSearchRule('playcount',2005)
-        self.compare_rules(library, OrSearchRule([lt2,gt2]), rng2, "or")
+
+        rules = [ PartialStringSearchRule('artist','art1'),
+                  InvertedPartialStringSearchRule('artist','art1'),
+                  ExactSearchRule('artist','art1'),
+                  ExactSearchRule('playcount',2000),
+                  InvertedExactSearchRule('artist','art1'),
+                  InvertedExactSearchRule('playcount',2000),
+                  rng1, rng2, gt1, gt2, lt1, lt2]
+
+        for i, rule in enumerate(rules):
+            test_name = "test_rule_%d" % i
+            dict[test_name] = gen_compare_test(rule)
+
+        dict["test_and"] = gen_compare_rule_test(AndSearchRule([gt1,lt1]), rng1)
+        dict["test_or"] = gen_compare_rule_test(OrSearchRule([lt2,gt2]), rng2)
+
+        return type.__new__(cls, name, bases, dict)
+
+class TestSearch(unittest.TestCase):
+    __metaclass__ = TestSearchMeta
+
+    # python 2.7, 3.2 only, could be a better way to set up the db
+
+    #@classmethod
+    #def setUpClass(cls):
+    #    pass
+
+    #@classmethod
+    #def tearDownClass(cls):
+    #    pass
