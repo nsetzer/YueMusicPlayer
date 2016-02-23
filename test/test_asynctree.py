7cff96ca9007ffd2991f8000e0ed634af1179533
Nick Setzer <nsetzer@ccis.neu.edu>
2016-02-23 15:41:21 -0500

    remove demo function

--- test/test_asynctree.py
+++ test/test_asynctree.py
@@ -48,52 +48,6 @@ def main():
 
 
 
-def convert():
-    """ convert from old style library to new style library """
-    from Song_XMLFormat import SongXML
-    from Song_Object import EnumSong
-    from SystemDateTime import DateTime
-
-    db_path = "./libimport.db"
-    sqlstore = SQLStore(db_path)
-    library = Library( sqlstore )
-
-    columns = {
-        EnumSong.UID        : "uid",
-        EnumSong.PATH       : "path",
-        EnumSong.ARTIST     : "artist",
-        EnumSong.COMPOSER   : "composer",
-        EnumSong.ALBUM      : "album",
-        EnumSong.TITLE      : "title",
-        EnumSong.GENRE      : "genre",
-        EnumSong.DATESTAMP  : "last_played",
-        EnumSong.COMMENT    : "comment",
-        EnumSong.LANG       : "lang",
-        EnumSong.COUNTRY    : "country",
-        EnumSong.RATING     : "rating",
-        EnumSong.LENGTH     : "length",
-        EnumSong.SONGINDEX  : "album_index",
-        EnumSong.PLAYCOUNT  : "playcount",
-        EnumSong.YEAR       : "year",
-    }
-
-    xml = r"D:\Dropbox\ConsolePlayer\user\music.xml"
-    lib = SongXML().read(xml)
-
-    if len(library) == 0:
-        for idx, old_song in enumerate(lib):
-            print("processing %d/%d - %d"%(idx,len(lib),old_song[EnumSong.UID]))
-            new_song = {}
-            for field,column in columns.items():
-                new_song[column] = old_song[field]
-            library.insert(**new_song)
-
-    dt = DateTime(format)
-    dt.timer_start()
-    songlist = list(library.iter())
-    dt.timer_end();
-
-    sys.stderr.write("Loaded %d songs from sqlite container in %s\n"%( len(songlist), DateTime.formatTimeDeltams(dt.timedelta)))
 
 
 
