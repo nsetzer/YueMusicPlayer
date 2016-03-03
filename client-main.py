fd384a321430238fe24cb0f136a28f144d7080f5
Nick Setzer <nsetzer@ccis.neu.edu>
2016-03-02 20:30:22 -0500

    write start up errors to log file

--- client-main.py
+++ client-main.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python
 __version__ = "1.0"
 
-from yue.client.client import main
-
-main()
+import traceback
+try:
+    from yue.client.client import main
+    main()
+except Exception as e:
+    with open("error.log","w") as f:
+        f.write("%s\n"%e)
+        f.write(traceback.format_exc())
