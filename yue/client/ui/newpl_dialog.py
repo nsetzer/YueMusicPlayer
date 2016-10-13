d9cbc1929f3ffb855e75d1d0ef6b3aa51076fffc
Nick Setzer <nsetzer@ccis.neu.edu>
2016-10-13 19:52:19 -0400

    simply new playlist dialog

--- yue/client/ui/newpl_dialog.py
+++ yue/client/ui/newpl_dialog.py
@@ -15,10 +15,10 @@ class NewPlaylistDialog(QDialog):
 
         # --------------------------
         # Widgets
-        self.rad_all     = QRadioButton("All Music",self)
-        self.rad_preset  = QRadioButton("Preset Search",self)
-        self.rad_preset.setDisabled(True)
-        self.rad_custom  = QRadioButton("Custom Search:",self)
+        #self.rad_all     = QRadioButton("All Music",self)
+        #self.rad_preset  = QRadioButton("Preset Search",self)
+        #self.rad_preset.setDisabled(True)
+        #self.rad_custom  = QRadioButton("Custom Search:",self)
 
         self.edit         = LineEdit(self)
 
@@ -28,27 +28,28 @@ class NewPlaylistDialog(QDialog):
         self.chk_today   = QCheckBox("Ignore Songs Played Today",self)
         self.chk_ban     = QCheckBox("Ignore Bannished Songs",self)
 
-        self.cbox_preset = QComboBox(self)
+        #self.cbox_preset = QComboBox(self)
         self.spin_size   = QSpinBox(self)
         #self.spin_hash   = QSpinBox(self)
 
         # --------------------------
         # Default Values
 
+        self.edit.setReadOnly(True)
         self.edit.setPlaceholderText("All Music")
 
         if query:
-            self.rad_custom.setChecked(True)
+            #self.rad_custom.setChecked(True)
             self.edit.setText(query)
-        else:
-            self.rad_all.setChecked(True)
+        #else:
+        #    self.rad_all.setChecked(True)
 
         self.chk_ban.setChecked(True)
 
         #self.spin_hash.setRange(0,100);
         self.spin_size.setRange(10,200);
 
-        self.cbox_preset.setDisabled(True);
+        #self.cbox_preset.setDisabled(True);
 
         #self.spin_hash.setValue(0);
         self.spin_size.setValue(limit);
@@ -58,15 +59,15 @@ class NewPlaylistDialog(QDialog):
         self.grid = QGridLayout(self)
 
         row = 0
-        self.grid.addWidget(self.rad_all   ,row,0,Qt.AlignLeft)
-        row+=1;
-        self.grid.addWidget(self.rad_preset,row,0,Qt.AlignLeft)
+        #self.grid.addWidget(self.rad_all   ,row,0,Qt.AlignLeft)
+        #row+=1;
+        #self.grid.addWidget(self.rad_preset,row,0,Qt.AlignLeft)
         #self.grid.addWidget(QLabel("Preset Number:"),row,1,Qt.AlignRight)
-        self.grid.addWidget(self.cbox_preset,row,1,1,2,Qt.AlignRight)
-        row+=1;
-        self.grid.addWidget(self.rad_custom,row,0,Qt.AlignLeft)
+        #self.grid.addWidget(self.cbox_preset,row,1,1,2,Qt.AlignRight)
+        #row+=1;
+        #self.grid.addWidget(self.rad_custom,row,0,Qt.AlignLeft)
 
-        row+=1; # self.edit ROW
+        #row+=1; # self.edit ROW
         vl_edit = QVBoxLayout(); # layout allows self.edit to expand
         self.grid.addLayout(vl_edit,row,0,1,3)#       widget,row,col,row_span,col_span
         vl_edit.addWidget(self.edit)
@@ -91,46 +92,52 @@ class NewPlaylistDialog(QDialog):
 
         # --------------------------
         # connect signals
-        self.rad_all.clicked.connect(self.event_click_radio_button_1)
-        self.rad_preset.clicked.connect(self.event_click_radio_button_2)
-        self.rad_custom.clicked.connect(self.event_click_radio_button_3)
+        #self.rad_all.clicked.connect(self.event_click_radio_button_1)
+        #self.rad_preset.clicked.connect(self.event_click_radio_button_2)
+        #self.rad_custom.clicked.connect(self.event_click_radio_button_3)
         #self.edit.textEdited.connect(self.event_text_changed)
         self.edit.keyPressEvent = self.event_text_changed
 
         self.btn_accept.clicked.connect(self.accept)
         self.btn_cancel.clicked.connect(self.reject)
 
-        self.cbox_preset.currentIndexChanged.connect(self.event_cbox_preset_changed)
+        #self.cbox_preset.currentIndexChanged.connect(self.event_cbox_preset_changed)
 
         self.resize(480,240)
 
     def setPresets(self,names,queries):
-        self.rad_preset.setEnabled(True)
-        self.cbox_preset.clear();
-        for n,q in sorted(zip(names,queries),key=lambda x:x[0]):
-            self.cbox_preset.addItem(n,q)
-        self.cbox_preset.setCurrentIndex(0);
+        #self.rad_preset.setEnabled(True)
+        #self.cbox_preset.clear();
+        #for n,q in sorted(zip(names,queries),key=lambda x:x[0]):
+        #    self.cbox_preset.addItem(n,q)
+        #self.cbox_preset.setCurrentIndex(0);
+        return
 
     def event_click_radio_button_1(self,event=None):
-        self.cbox_preset.setDisabled(True);
-        self.edit.setText("");
+        #self.cbox_preset.setDisabled(True);
+        #self.edit.setText("");
+        return
 
     def event_click_radio_button_2(self,event=None):
-        self.cbox_preset.setDisabled(False);
-        self.event_cbox_preset_changed(self.cbox_preset.currentIndex())
+        #self.cbox_preset.setDisabled(False);
+        #self.event_cbox_preset_changed(self.cbox_preset.currentIndex())
+        return
 
     def event_click_radio_button_3(self,event=None):
-        self.cbox_preset.setDisabled(True);
+        #self.cbox_preset.setDisabled(True);
+        return
 
     def event_text_changed(self,event=None):
         super(LineEdit,self.edit).keyPressEvent(event)
-        self.rad_custom.setChecked(True)
-        self.cbox_preset.setDisabled(True);
+        #self.rad_custom.setChecked(True)
+        #self.cbox_preset.setDisabled(True);
+        return
 
     def event_cbox_preset_changed(self, value):
-        if self.rad_preset.isChecked():
-            text = self.cbox_preset.itemData(value)
-            self.edit.setText(text);
+        #if self.rad_preset.isChecked():
+        #    text = self.cbox_preset.itemData(value)
+        #    self.edit.setText(text);
+        return
 
     def getQueryParams(self):
 
